b5bf095037bb75b355da16ebd765dcc000ad55d3
Gyanmaurya <gyanprakashme0051@gmail.com>
2022-04-07 01:06:37 +0530

    webpack some part

---
+++ src/index.js
@@ -0,0 +1,9 @@
+ 
+ import "./index.css"
+//  import './img/file.jpg';
+
+ const root =document.getElementById("root");
+ const note=document.createElement("textarea");
+ note.classList.add('text')
+ note.innerText="write here";
+ root.appendChild(note)
